0819e516e4459e307b5a711a36e354a9af8fcfbe
Kelly Herron <kherron@luc.edu>
2017-11-18 18:58:54 -0600

    added toolbar at top of flashcard list for editing and adding, replaced previous add button in navigation bar with quiz button

--- Boost/Boost/Flashcard/FlashcardGroupTableViewController.swift
+++ Boost/Boost/Flashcard/FlashcardGroupTableViewController.swift
@@ -24,11 +24,6 @@ class FlashcardGroupTableViewController: UITableViewController {
         loadSampleFlashcardGroups()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
