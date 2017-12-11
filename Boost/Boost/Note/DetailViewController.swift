3d26d4db1edefaf8911c3c9c04442af69abf3d02
Kelly Herron <kherron@luc.edu>
2017-12-11 15:33:46 -0600

    scroll textView in notes so that text is not hidden by keyboard

--- Boost/Boost/Note/DetailViewController.swift
+++ Boost/Boost/Note/DetailViewController.swift
@@ -20,6 +20,10 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
         textView.text = text
         textView.becomeFirstResponder()
+        
+        //register for keyboard notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeShown(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
     
     func setText(t:String) {
@@ -41,6 +45,22 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: Keyboard Management
+    @objc func keyboardWillBeShown(_ sender: Notification) {
+        //scroll so text is not hidden by keyboard
+        let info = (sender as NSNotification).userInfo! as NSDictionary
+        let keyboardSize = (info.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsetsMake(0, 0, keyboardSize.height - view.safeAreaInsets.bottom, 0)
+        textView.contentInset = contentInsets
+        textView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillBeHidden(_ sender: Notification) {
+        //scroll to bottom of textView
+        let contentInsets = UIEdgeInsets.zero
+        textView.contentInset = contentInsets
+        textView.scrollIndicatorInsets = contentInsets
+    }
 
     /*
     // MARK: - Navigation
